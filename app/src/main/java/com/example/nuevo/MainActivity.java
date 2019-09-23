cae9d690d313ecc8285e601eb3c1a6355123356b
Felix Antonio Flores <floresfa1511@gmail.com>
2019-09-23 20:37:58 -0300

    Actualizacion 23/9/19

--- app/src/main/java/com/example/nuevo/MainActivity.java
+++ app/src/main/java/com/example/nuevo/MainActivity.java
@@ -6,18 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button comenzar;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.grow_anim);
         comenzar = (Button)(findViewById(R.id.play));
+        comenzar.setAnimation(animation);
         comenzar.setOnClickListener(MainActivity.this);
 
     }
--- app/src/main/java/com/example/nuevo/Menu01.java
+++ app/src/main/java/com/example/nuevo/Menu01.java
@@ -5,25 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class Menu01 extends AppCompatActivity implements View.OnClickListener {
 
     ImageButton btn_rutina;
     ImageButton btn_agenda;
     ImageButton btn_oraciones;
+    ImageView img_corazon,img_reloj,img_lapiz;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu01);
 
-        btn_rutina = (ImageButton) (findViewById(R.id.imageButton7));
+
+        Animation anim_scale = AnimationUtils.loadAnimation(this, R.anim.grow_anim);
+        Animation anim_shake = AnimationUtils.loadAnimation(this, R.anim.shake_anim);
+        img_corazon = (ImageView) findViewById(R.id.corazon);
+        img_reloj = (ImageView) findViewById(R.id.reloj);
+        img_lapiz= (ImageView) findViewById(R.id.lapiz);
+        btn_rutina = (ImageButton) (findViewById(R.id.btn_rutina));
         //btn_rutina.setOnClickListener(Menu01.this);
+        //btn_rutina.startAnimation(anim_scale);
+        img_corazon.startAnimation(anim_scale);
+        img_reloj.startAnimation(anim_shake);
+        img_lapiz.startAnimation(anim_scale);
+        btn_agenda = (ImageButton) (findViewById(R.id.brn_agenda));
+        //btn_agenda.startAnimation(anim_shake);
 
-        btn_agenda = (ImageButton) (findViewById(R.id.imageButton6));
-
-        btn_oraciones = (ImageButton) (findViewById(R.id.imageButton5));
+        btn_oraciones = (ImageButton) (findViewById(R.id.btn_oraciones));
 
         btn_rutina.setOnClickListener(new View.OnClickListener() {
             @Override
