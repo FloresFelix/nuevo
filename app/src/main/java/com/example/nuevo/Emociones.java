9eade60fd0cbb25f514f429cd982b9109b304918
FloresFelix <52514937+FloresFelix@users.noreply.github.com>
2020-02-12 22:47:17 -0300

    commit 12-2-2020 - programado el adapter

--- app/src/main/java/com/example/nuevo/Emociones.java
+++ app/src/main/java/com/example/nuevo/Emociones.java
@@ -5,11 +5,34 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.gigamole.infinitecycleviewpager.HorizontalInfiniteCycleViewPager;
+
+import java.util.ArrayList;
+
 public class Emociones extends AppCompatActivity {
+
+
+    ArrayList<ItemPictograma> pictogramas;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.emociones);
+        pictogramas = new ArrayList<>();
+        LlenarDatos();
+        HorizontalInfiniteCycleViewPager horizontalInfiniteCycleViewPager = findViewById(R.id.cards);
+        AdapterPictograma adapterPictograma = new AdapterPictograma(pictogramas,getApplicationContext());
+        horizontalInfiniteCycleViewPager.setAdapter(adapterPictograma);
+    }
+
+
+    void  LlenarDatos()
+    {
+        pictogramas.add(new ItemPictograma(R.drawable.carabonton1,"Felixz"));
+        pictogramas.add(new ItemPictograma(R.drawable.caraboton2,"Triste"));
+        pictogramas.add(new ItemPictograma(R.drawable.carafeliz,"Pensativo"));
+        pictogramas.add(new ItemPictograma(R.drawable.caratranquilo,"Tranquilo"));
+
+
     }
 }
