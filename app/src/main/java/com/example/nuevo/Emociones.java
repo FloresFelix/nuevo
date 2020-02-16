5357cda978e1e5bdc95c1e939cde0266f4e6bce5
FloresFelix <52514937+FloresFelix@users.noreply.github.com>
2020-02-16 01:36:58 -0300

    commit 16-2-2020 agregado las apciones alimentacion, higiene y recreatividad

--- app/src/main/java/com/example/nuevo/Emociones.java
+++ app/src/main/java/com/example/nuevo/Emociones.java
@@ -1,9 +1,12 @@
 package com.example.nuevo;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.res.ResourcesCompat;
 
 import com.gigamole.infinitecycleviewpager.HorizontalInfiniteCycleViewPager;
 
@@ -13,12 +16,14 @@ public class Emociones extends AppCompatActivity {
 
 
     ArrayList<ItemPictograma> pictogramas;
+    ImageView titulo;
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.emociones);
+        titulo = findViewById(R.id.titulo);
         pictogramas = new ArrayList<>();
         LlenarDatos();
         HorizontalInfiniteCycleViewPager horizontalInfiniteCycleViewPager = findViewById(R.id.cards);
@@ -29,11 +34,49 @@ public class Emociones extends AppCompatActivity {
 
     void  LlenarDatos()
     {
-        pictogramas.add(new ItemPictograma(R.drawable.carabonton1,"Felixz"));
-        pictogramas.add(new ItemPictograma(R.drawable.caraboton2,"Triste"));
-        pictogramas.add(new ItemPictograma(R.drawable.carafeliz,"Pensativo"));
-        pictogramas.add(new ItemPictograma(R.drawable.caratranquilo,"Tranquilo"));
+        switch (getIntent().getIntExtra("opcion",10)){
 
+            case 1:{
+                titulo.setImageResource(R.drawable.emociones);
+                pictogramas.add(new ItemPictograma(R.drawable.carabonton1,"Felixz"));
+                pictogramas.add(new ItemPictograma(R.drawable.caraboton2,"Triste"));
+                pictogramas.add(new ItemPictograma(R.drawable.carafeliz,"Pensativo"));
+                pictogramas.add(new ItemPictograma(R.drawable.caratranquilo,"Tranquilo"));
+                break;
+            }
+            case 2:{
+                titulo.setImageResource(R.drawable.alimentacion);
+                pictogramas.add(new ItemPictograma(R.drawable.carabonton1,"Felixz"));
+                pictogramas.add(new ItemPictograma(R.drawable.caraboton2,"Triste"));
+                pictogramas.add(new ItemPictograma(R.drawable.carafeliz,"Pensativo"));
+                pictogramas.add(new ItemPictograma(R.drawable.caratranquilo,"Tranquilo"));
+                break;
+            }
+            case 3:{
+                titulo.setImageResource(R.drawable.higiene);
+                pictogramas.add(new ItemPictograma(R.drawable.carabonton1,"Felixz"));
+                pictogramas.add(new ItemPictograma(R.drawable.caraboton2,"Triste"));
+                pictogramas.add(new ItemPictograma(R.drawable.carafeliz,"Pensativo"));
+                pictogramas.add(new ItemPictograma(R.drawable.caratranquilo,"Tranquilo"));
+                break;
+            }
+            case 4:{
+                titulo.setImageResource(R.drawable.recreatividad);
+                pictogramas.add(new ItemPictograma(R.drawable.carabonton1,"Felixz"));
+                pictogramas.add(new ItemPictograma(R.drawable.caraboton2,"Triste"));
+                pictogramas.add(new ItemPictograma(R.drawable.carafeliz,"Pensativo"));
+                pictogramas.add(new ItemPictograma(R.drawable.caratranquilo,"Tranquilo"));
+            }
+            default: break;
+        }
+
+    }
+
+    @Override
+    public void finish()
+    {
+        super.finish();
+        overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right);
 
     }
 }
--- app/src/main/java/com/example/nuevo/Menu02.java
+++ app/src/main/java/com/example/nuevo/Menu02.java
@@ -7,9 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
-public class Menu02 extends AppCompatActivity {
+public class Menu02 extends AppCompatActivity implements View.OnClickListener {
 
     ImageButton btn_emociones;
+    ImageButton btn_alimentacion;
+    ImageButton btn_higiene;
+    ImageButton btn_recreatividad;
+
+     final int   OPCION_EMOCIONES       = 1;
+     final  int  OPCION_ALIMENTACION    = 2;
+     final int   OPCION_HIGIENE         = 3;
+     final int   OPCION_RECREATIVIDAD   = 4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +26,18 @@ public class Menu02 extends AppCompatActivity {
         setContentView(R.layout.activity_menu02);
 
         btn_emociones = (ImageButton) findViewById(R.id.btn_emociones);
+        btn_alimentacion = findViewById(R.id.btn_alimentacion);
+        btn_higiene = findViewById(R.id.btn_higiene);
+        btn_recreatividad = findViewById(R.id.btn_recreatividad);
 
+
+        btn_recreatividad.setOnClickListener(this);
+        btn_higiene.setOnClickListener(this);
+        btn_alimentacion.setOnClickListener(this);
+        btn_emociones.setOnClickListener(this);
+
+
+    /*
         btn_emociones.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,6 +46,7 @@ public class Menu02 extends AppCompatActivity {
                 overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
             }
         });
+     */
     }
 
 
@@ -34,6 +55,46 @@ public class Menu02 extends AppCompatActivity {
     {
         super.finish();
         overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId())
+        {
+            case R.id.btn_emociones: {
+                Intent intent = new Intent(Menu02.this, Emociones.class);
+                intent.putExtra("opcion",OPCION_EMOCIONES);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                break;
+            }
+            case R.id.btn_alimentacion:{
+
+                Intent intent = new Intent(Menu02.this,Emociones.class);
+                intent.putExtra("opcion",OPCION_ALIMENTACION);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                break;
+            }
+            case R.id.btn_higiene:{
+                Intent intent = new Intent(Menu02.this,Emociones.class);
+                intent.putExtra("opcion",OPCION_HIGIENE);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                break;
+            }
+            case R.id.btn_recreatividad:{
+                Intent intent = new Intent(Menu02.this,Emociones.class);
+                intent.putExtra("opcion",OPCION_RECREATIVIDAD);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                break;
+            }
+            default: break;
+
+        }
 
     }
 }
